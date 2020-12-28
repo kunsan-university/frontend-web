84bb7ae6db16cdac3d1cd701daa7882340118d9f
ha <creaworpiece@gmail.com>
2020-12-28 20:14:14 +0900

    cors test

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,4 +2,4 @@ import axios from 'axios';
 import axsios from 'axios';
 
 export const getUser = (id) =>
-  axios.get(`http://localhost:8082/board/BBSMSTR_AAAAAAAAAAAA`);
+  axios.get(`http://junho-dev.tk:8082/board/BBSMSTR_AAAAAAAAAAAA`);
